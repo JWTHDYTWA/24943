beda6384b33f60a09f1b167447afdbecb91a2c9a
Andrei Chervov <a.chervov@g.nsu.ru>
2025-12-10 23:21:12 +0700

    Task 21.

--- AChervov/21.c
+++ AChervov/21.c
@@ -4,31 +4,62 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <signal.h>
+
+const char ctrl_g = 0x07;
 
 struct termios term;
 struct termios term_bak;
-int restore_bak = 0;
+char buf[16];
+volatile sig_atomic_t bell_cnt;
+volatile sig_atomic_t stop;
 
 void restore_term()
 {
-    if (restore_bak)
-    {
-        tcsetattr(0, TCSAFLUSH, &term_bak);
-    }
+    tcsetattr(0, TCSAFLUSH, &term_bak);
 }
 
 void init_term(int fd)
 {
     tcgetattr(fd, &term);
     term_bak = term;
-    restore_bak = 1;
     atexit(restore_term);
     term.c_lflag &= ~(ECHO|ICANON);
     tcsetattr(fd, TCSAFLUSH, &term);
 }
 
+void signal_handler(int sig)
+{
+    if (sig == SIGINT)
+    {
+        write(STDOUT_FILENO, &ctrl_g, 1);
+        bell_cnt++;
+    }
+    else if (sig == SIGQUIT)
+    {
+        stop = 1;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     init_term(0);
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+
+    bell_cnt = 0;
+    stop = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGQUIT, &sa, NULL);
+
+    while (!stop)
+    {
+        pause();
+    }
+
+    printf("Bell has been triggered %d times.\n", bell_cnt);
+
     return 0;
 }
